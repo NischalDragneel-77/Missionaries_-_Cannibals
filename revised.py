006c8b67344ba7ec72a6c47e952826dcae1b876f
DDragneel <nbhn333@gmail.com>
2022-08-19 20:46:54 +0545

    finalized revised

--- revised.py
+++ revised.py
@@ -15,7 +15,6 @@ def isValidState(node):
    return (node.left_bank[0]>=node.left_bank[1] and node.right_bank[0]>=node.right_bank[1]) or (node.left_bank[0]==0 or node.right_bank[0]==0)
 
 def generateChildren(parent,move):
-   # no_of_nodes += 1
    newState = {
             'left_bank':[0,0],
             'right_bank':[0,0],
@@ -48,10 +47,6 @@ class Nodes:
       self.leaf = False
       self.index = index
       
-   # def __repr__(self):
-   #    return str(self.presentState)
-   
-
 if __name__=="__main__":
    nodes_generated = 0
    q=deque()
@@ -66,29 +61,24 @@ if __name__=="__main__":
    g.vs[nodes_generated]['name'] = root.index
    while q:
       node = q.popleft()
-      
       if node.presentState == [3,3,1] or node.presentState == [0,0,0]:
          node.status = 'goal'
          node.color = 'green'
          pastStates.append(node.presentState)
          node.leaf = True
          continue
-
       if not isValidState(node):
          node.status = 'invalid'
          node.color = 'red'
          pastStates.append(node.presentState)
          node.leaf = True
          continue
-
       if pastStates.count(node.presentState):
          node.status = 'repeated'
          node.color = 'blue'
          pastStates.append(node.presentState)
          node.leaf = True
          continue
-
-
       pastStates.append(node.presentState)
       if (node.status != 'invalid' or node.status != 'repeated'):
          possibleMoves = validMove(node)
@@ -102,11 +92,8 @@ if __name__=="__main__":
             g.vs[nodes_generated]['name'] = child.index
             v1 = g.vs.find(name=node.index)
             v2 = g.vs.find(name = child.index)
-            print(v1,v2)
             g.add_edge(v1,v2)
             g.es[g.get_eid(v1,v2)]['label'] = str(i)
-   print(len(pastStates))
-   print(g)
    layout = g.layout('tree')
    visual_style = {}
    visual_style["vertex_size"] = 50
@@ -115,7 +102,7 @@ if __name__=="__main__":
    visual_style["vertex_color"] = [nodes.color for nodes in g.vs['nodes']]
    visual_style["vertex_label"] = [nodes.presentState for nodes in g.vs['nodes']]
    visual_style["edge_label"] = g.es['label']
-   # visual_style["edge_width"] = [1 + 2 * int(is_formal) for is_formal in g.es["is_formal"]]
    visual_style["layout"] = layout
-   visual_style["bbox"] = (1920, 1080)
+   visual_style["bbox"] = (1080, 1080)
+   visual_style["margin"] = 40
    plot(g,"statespacetree.png", **visual_style)
